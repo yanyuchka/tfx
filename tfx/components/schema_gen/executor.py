6fc4af188673fe171804fb69b6feaf96a053691d
yanyuchka <yanyuchka@gmail.com>
2020-12-24 17:18:14 -0500

    Use TFRecords file pattern in SchemaGen
    (via `get_only_uri_in_dir` new param)

--- tfx/components/schema_gen/executor.py
+++ tfx/components/schema_gen/executor.py
@@ -93,7 +93,7 @@ class Executor(base_executor.BaseExecutor):
 
       logging.info('Processing schema from statistics for split %s.', split)
       stats_uri = io_utils.get_only_uri_in_dir(
-          os.path.join(stats_artifact.uri, split))
+          os.path.join(stats_artifact.uri, split), io_utils.TFRECORD_FILES_PATTERN)
       if not schema:
         schema = tfdv.infer_schema(
             tfdv.load_statistics(stats_uri), infer_feature_shape)
